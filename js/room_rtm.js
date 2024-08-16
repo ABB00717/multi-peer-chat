059429429e25dd3d3dae9953779257a7af553c0f
ABB00717 <eric94528@gmail.com>
2024-08-16 13:11:53 +0800

    Implement participants list

--- js/room_rtm.js
+++ js/room_rtm.js
@@ -1,3 +1,14 @@
 let handleMemberJoined = async (memberId) => {
   console.log('Member joined:', memberId);
+  addMemberToDom(memberId);
+};
+
+let addMemberToDom = async (memberId) => {
+  let membersWrapper = document.getElementById(`member__list`);
+  let memberItem = `<div class="member__wrapper" id="member__${memberId}__wrapper">
+                      <span class="green__icon"></span>
+                      <p class="member_name">${memberId}</p>
+                    </div>`;
+
+  membersWrapper.insertAdjacentHTML('beforeend', memberItem);
 };
