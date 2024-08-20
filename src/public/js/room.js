da0fd22303258cbdc034daa96973acd6b5748960
ABB00717 <eric94528@gmail.com>
2024-08-20 13:01:40 +0800

    Click to hide stream__box

--- src/public/js/room.js
+++ src/public/js/room.js
@@ -57,8 +57,22 @@ for (let i = 0; videoFrames.length > i; i++) {
   videoFrames[i].addEventListener('click', expandVideoFrame);
 }
 
+let hideDisplayFrame = () => {
+  userIdDisplayFrame = null;
+  displayFrame.style.display = null;
+
+  let child = displayFrame.children[0];
+  document.getElementById('streams__container').appendChild(child);
+  
+  for (let i = 0; videoFrames.length > i; i++) {
+    videoFrames[i].style.height = '300px';
+    videoFrames[i].style.width = '300px';
+  }
+};
+
 let leaveChannel = () => {
   socket.emit('leave', roomId);
 };
 
 window.addEventListener('beforeunload', leaveChannel);
+displayFrame.addEventListener('click', hideDisplayFrame);
